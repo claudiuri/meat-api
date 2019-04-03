29e7d6af11e95b41e719dfb7c6895094a22b5109
Cláudio Yuri <claudio.y07@gmail.com>
2019-04-02 21:39:38 -0300

    criação do schema de restaurante e do menu

---
+++ dist/restaurants/restaurants.model.js
@@ -0,0 +1,25 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose = require("mongoose");
+const menuSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+});
+const restSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    menu: {
+        type: [menuSchema],
+        required: false,
+        select: false
+    }
+});
+exports.Restaurant = mongoose.model('Restaurant', restSchema);
