76299d8478c44e3d6e00ae2c43fc0a2bedf92d0b
Cláudio Yuri <claudio.y07@gmail.com>
2019-04-04 22:37:11 -0300

    criação da hypermedia

--- dist/reviews/reviews.router.js
+++ dist/reviews/reviews.router.js
@@ -6,14 +6,20 @@ class ReviewsRouter extends model_router_1.ModelRouter {
     constructor() {
         super(reviews_model_1.Review);
     }
+    envelope(document) {
+        let resource = super.envelope(document);
+        const restId = document.restaurant._id ? document.restaurant._id : document.restaurant;
+        resource._links.restaurant = `/restaurants/${restId}`;
+        return resource;
+    }
     prepareOne(query) {
         return query.populate('user', 'name')
             .populate('restaurant', 'name');
     }
     applyRoutes(application) {
-        application.get('/reviews', this.findAll);
-        application.get('/reviews/:id', [this.validateId, this.findById]);
-        application.post('/reviews', this.save);
+        application.get(`${this.basePath}`, this.findAll);
+        application.get(`${this.basePath}/:id`, [this.validateId, this.findById]);
+        application.post(`${this.basePath}`, this.save);
     }
 }
 exports.reviewsRouter = new ReviewsRouter();
